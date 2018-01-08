53afc45b67d8b6f9b7c13f0472e50de2cc60b1c0
Katie D'Angelo <kd.dangelo@gmail.com>
2018-01-08 18:03:28 -0500

    media queries added

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -7,7 +7,7 @@ weatherApp.darkAPIUrl = 'https://api.darksky.net/forecast/fbec69199541239bcdb9ce
 // on click of button, start loading screen, begin getWeather
 weatherApp.buttonStart = function () {
     $('.weather-start--button').on('click', function (event) {
-        $('loader').slideDown('slow');
+        $('loader').slideDown('fast');
         weatherApp.getWeather();
         $('.pre-text--pre-results').hide();
     });
@@ -30,7 +30,7 @@ weatherApp.getWeather = function () {
 
         }).then(function (weatherInfo) {
 
-            weatherApp.displayWeatherOnPage(weatherInfo.currently, weatherInfo.alerts, weatherInfo.daily);
+            weatherApp.displayWeatherOnPage(weatherInfo.currently, weatherInfo.daily);
 
             weatherApp.convertTime(weatherInfo.currently.time);
         });
@@ -58,11 +58,9 @@ weatherApp.convertTime = function (unixTimestamp) {
 };
 
 // display weather on page
-weatherApp.displayWeatherOnPage = function (current, alerts, daily) {
-    console.log(current);
-    console.log(alerts);
+weatherApp.displayWeatherOnPage = function (current, daily) {
 
-    // round to 1 dec point for temperatures
+    // round with no dec point for temperatures
     var numberCurrent = current.temperature;
     var numberApparent = current.apparentTemperature;
 
@@ -73,28 +71,22 @@ weatherApp.displayWeatherOnPage = function (current, alerts, daily) {
     var percipProb = current.precipProbability;
 
     var percentagePercipProb = Math.round(percipProb * 100);
-    console.log(percentagePercipProb);
 
-    // meter to km
-    var windSpeedCurrent = current.windSpeed;
-    // console.log(windSpeedCurrent);
-    // const kmWindSpeedCurrent = Math.round(windSpeedCurrent/1000).toFixed(2);
-
-    $('.wind-speed--results').text(current.windSpeed);
-
+    // push info to DOM
     $('.summary--results').text(current.summary);
-    $('.temp-c-current--results').text(roundedCurrent);
-    $('.temp-c-feels-like--results').text(roundedApparent);
-    $('.daily-week--results').text(daily.summary);
-    $('.wind-speed--results').text(windSpeedCurrent);
+    $('.temp-c-current--results').text(roundedCurrent + '\xB0C');
+    $('.temp-c-feels-like--results').text(roundedApparent + '\xB0C');
+    $('.daily-week--results').text('' + daily.summary);
+    $('.wind-speed--results').text(current.windSpeed + ' m/sec');
+    $('.uv-index--results').text(current.uvIndex);
 
     // if/else for precip probability to show intensity or not
     if (percentagePercipProb === 0) {
-        $('.precip-prop--results').text(percentagePercipProb);
+        $('.precip-prop--results').text(percentagePercipProb + '%');
         $('.precip-details--results-p').hide();
     } else {
-        $('.precip-prop--results').text(percentagePercipProb);
-        $('.precip-instens--results').text(current.precipIntensity);
+        $('.precip-prop--results').text(percentagePercipProb + '%');
+        $('.precip-instens--results').text(current.precipIntensity + ' mil/hour');
         $('.precip-type--results').text(current.precipType);
     };
 
@@ -105,9 +97,8 @@ weatherApp.displayWeatherOnPage = function (current, alerts, daily) {
 weatherApp.changeBackgroundAndIcon = function (backgroundNew) {
     var icons = new Skycons({ "color": "rgb(24, 29, 37)" });
 
+    // play icons
     icons.play();
-
-    console.log(backgroundNew);
 
     if (backgroundNew === "clear-day") {
         $('main').css('background', 'url("/public/styles/assets/clear-day--background.jpg")');
@@ -115,21 +106,30 @@ weatherApp.changeBackgroundAndIcon = function (backgroundNew) {
     } else if (backgroundNew === "clear-night") {
         $('main').css('background', 'url("/public/styles/assets/clear-night--background.jpg")');
         icons.set("icon1", Skycons.CLEAR_NIGHT);
+        $('header, footer a').css({
+            'color': 'rgb(254, 255, 253)'
+        });
     } else if (backgroundNew === "fog") {
         $('main').css('background', 'url("/public/styles/assets/fog--background.jpg")');
         icons.set("icon1", Skycons.FOG);
+        $('footer a').css({
+            'color': 'rgb(254, 255, 253)'
+        });
     } else if (backgroundNew === "rain" || backgroundNew === "thunderstorm") {
         $('main').css('background', 'url("/public/styles/assets/rain--background.jpg")');
         icons.set("icon1", Skycons.RAIN);
+        $('header, footer a').css({
+            'color': 'rgb(254, 255, 253)'
+        });
     } else if (backgroundNew === "snow" || backgroundNew === "hail") {
         $('main').css('background', 'url("/public/styles/assets/snow--background.jpg")');
         icons.set("icon1", Skycons.SNOW);
     } else if (backgroundNew === "wind" || backgroundNew === "tornado") {
         $('main').css('background', 'url("/public/styles/assets/wind--background.jpg")');
         icons.set("icon1", Skycons.WIND);
-    } else if (backgroundNew === "sleet") {
-        $('main').css('background', 'url("/public/styles/assets/wind--background.jpg")');
-        icons.set("icon1", Skycons.SLEET);
+        $('header, footer a').css({
+            'color': 'rgb(254, 255, 253)'
+        });
     } else if (backgroundNew === "sleet") {
         $('main').css('background', 'url("/public/styles/assets/wind--background.jpg")');
         icons.set("icon1", Skycons.SLEET);
@@ -139,11 +139,14 @@ weatherApp.changeBackgroundAndIcon = function (backgroundNew) {
         icons.set("icon1", Skycons.CLOUDY);
         $('main').css({
             'background': 'url("/public/styles/assets/overcast--background.jpg")' });
+        $('header').css({
+            'color': 'rgb(254, 255, 253)'
+        });
     } else if (backgroundNew === "partly-cloudy-night") {
         $('main').css({
             'background': 'url("/public/styles/assets/clear-night--background.jpg")'
         });
-        $('footer a, h1, header .fa-umbrella').css({ 'color': 'rgb(254, 255, 253)' });
+        $('footer a, header').css({ 'color': 'rgb(254, 255, 253)' });
         icons.set("icon1", Skycons.PARTLY_CLOUDY_NIGHT);
     } else {
         $('main').css('background', 'url("/public/styles/assets/clear-day--background.jpg")');
@@ -158,6 +161,7 @@ weatherApp.changeBackgroundAndIcon = function (backgroundNew) {
     weatherApp.pageLoaded();
 };
 
+// once page is loaded what appears on page + loading page done
 weatherApp.pageLoaded = function () {
     $('.weather-start--button').html('Update Weather');
     $('.weather-results').show();
@@ -167,21 +171,25 @@ weatherApp.pageLoaded = function () {
     }, 500);
 };
 
+// holding click event
 weatherApp.events = function () {
-
     $('.fa-info-circle').click(function () {
-        $('.info-circle-hover--text').show();
-        $('.precip-prop--results-p').on('mouseleave', function () {
+        $('.info-circle-hover--text').show().css({
+            'top': '-22px'
+        });
+        $('.uv-index--mouse-leave').on('mouseleave', function () {
             $('.info-circle-hover--text').hide();
         });
     });
 };
 
+// init 
 weatherApp.init = function () {
     this.buttonStart();
     this.events();
 };
 
+// doc ready
 $(document).ready(function () {
     weatherApp.init();
 });
